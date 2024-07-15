df23442500756efcafa05ec2905a5fb7b0915796
dev-hos <h.dev.mo@gmail.com>
2024-07-15 20:54:08 +0200

    📦 NEW:  Add form && recipe crud

--- src/Controller/RecipesController.php
+++ src/Controller/RecipesController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Recipes;
+use App\Form\RecipeType;
 use App\Repository\RecipesRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
@@ -12,7 +15,7 @@ use Symfony\Component\Routing\Requirement\Requirement;
 #[Route('/recipes', name: 'app_recipes_')]
 class RecipesController extends AbstractController
 {
-    #[Route('/', name: 'index')]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(RecipesRepository $recipesRepository): Response
     {
         return $this->render('recipes/index.html.twig', [
@@ -20,7 +23,7 @@ class RecipesController extends AbstractController
         ]);
     }
 
-    #[Route('/{slug}-{id}', name: 'show', requirements: ['slug' => '[a-z0-9-]+', 'id' => '\d+'])]
+    #[Route('/{slug}-{id}', name: 'show', requirements: ['slug' => '[a-z0-9-]+', 'id' => '\d+'], methods: ['GET'])]
     public function show(Recipes $Recipe): Response
     {
         return $this->render('recipes/show.html.twig', [
@@ -30,4 +33,52 @@ class RecipesController extends AbstractController
             'recipe' => $Recipe,
         ]);
     }
+
+    #[Route('/new', name: 'create', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $recipe = new Recipes();
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($recipe);
+            $em->flush();
+            $this->addFlash('success', 'La recette a bien été créée');
+            return $this->redirectToRoute('app_recipes_index');
+        }
+
+        return $this->render('recipes/add.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit', requirements: ['slug' => '[a-z0-9-]+', 'id' => '\d+'], methods: ['GET', 'POST'])]
+    public function edit(Recipes $Recipe, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(RecipeType::class, $Recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'La recette a bien été modifiée');
+            return $this->redirectToRoute('app_recipes_index');
+        }
+
+        return $this->render('recipes/edit.html.twig', [
+            'recipe' => $Recipe,
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/{id}', name: 'delete', requirements: ['id' => Requirement::DIGITS], methods: ['DELETE'])]
+    public function delete (Recipes $recipes, EntityManagerInterface $em): Response
+    {
+        $em->remove($recipes);
+        $em->flush();
+        $this->addFlash('success', 'La recette a bien été supprimée');
+        return $this->redirectToRoute('app_recipes_index');
+    }
+
+
 }
---
+++ src/Form/RecipeType.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Recipes;
+use DateTimeZone;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Event\PostSubmitEvent;
+use Symfony\Component\Form\Event\PreSubmitEvent;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class RecipeType extends AbstractType
+{
+    public function __construct(private readonly SluggerInterface $slugger)
+    {
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('slug', TextType::class, [
+                'required' => false
+            ])
+            ->add('content')
+            ->add('duration')
+            ->addEventListener(FormEvents::PRE_SUBMIT, $this->autoSlug(...))
+            ->addEventListener(FormEvents::POST_SUBMIT, $this->timestamp(...))
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer',
+            ]);
+    }
+
+    private function autoSlug(PreSubmitEvent $event): void
+    {
+
+        $data = $event->getData();
+        if (empty($data['slug'])) {
+            $data['slug'] = strtolower($this->slugger->slug($data['title']));
+            $event->setData($data);
+        }
+    }
+
+    private function timestamp(PostSubmitEvent $postSubmitEvent): void
+    {
+        $timezone = new DateTimeZone('Europe/Paris');
+        $data = $postSubmitEvent->getData();
+        $data->setUpdatedAt(new \DateTimeImmutable('now', $timezone));
+        if (!$data->getId()) {
+            $data->setCreatedAt(new \DateTimeImmutable('now', $timezone));
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Recipes::class,
+        ]);
+    }
+}
